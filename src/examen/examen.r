ca233af78d40607c864b4227ddc35c125213b66d
Jose Valdes <josvaldes@gmail.com>
2023-05-31 17:23:58 -0500

    actualizacionHT

--- src/examen/examen.r
+++ src/examen/examen.r
@@ -10,7 +10,7 @@ gc()             #garbage collection
 require("data.table")
 
 #Parametros del script
-kexp_input  <- "HT8510"   #Aqui debe ir SU  bayesiana elegida
+kexp_input  <- "HT7510Semillerios"   #Aqui debe ir SU  bayesiana elegida
 # FIN Parametros del script
 
 #------------------------------------------------------------------------------
